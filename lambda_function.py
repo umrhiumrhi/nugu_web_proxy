be64c3430cb877e668e74df487eeef63cbb6e66d
최인석님/Talent 부 <skt.1520945@sktelecom.com>
2022-08-01 18:31:07 +0900

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,77 @@
+import json
+import os
+from num_for_check import *
+from similarity_checker import *
+
+def lambda_handler(event, context):
+    print("[original]: ",event)
+    
+    # post 방식에 의해 event로 넘겨 받았을 때 'body' key 참조
+    event = json.loads(event['body'])
+    print("[json]: ",event)
+    
+    appName = event["action"]['parameters']['appName']['value']
+    appExec = event['action']['parameters']['appExec']['value']
+    # searchKeyword = event['searchKeyword']
+    searchKeyword = ""
+    
+    resolve_info_list = event["context"]["supportedInterfaces"]["Extension"]["data"]["applicationList"]
+    print("[resolve_info_list] ", resolve_info_list)
+    
+    max_similarity = 0
+    most_similar_app_name = ""
+    most_similar_idx = -1
+    
+    for idx, resolve_info in enumerate (resolve_info_list) :
+        app_name_compared = resolve_info["appName"]
+        similarity = similarity_checker().find_similarity(appName, app_name_compared)
+        
+        if similarity >= MATCH_THRESHOLD and similarity > max_similarity:
+            max_similarity = similarity
+            most_similar_app_name = app_name_compared
+            most_similar_idx = idx
+            if similarity >= COMPLETE_MATCH : break
+    
+    # targetUri = getExecUri(appExec, appName, searchKeyword) 
+    targetUri = getExecUri(most_similar_idx, resolve_info_list)
+    
+    if appExec == "검색":
+        searchKeyword = event['searchKeyword']
+    
+    json_object = {
+      "version": "2.0",
+      "resultCode": "OK",
+      "output": {
+         "appName": app_name_compared,
+         "appExec": appExec,
+    "searchKeyword": searchKeyword
+      },
+      "directives": [
+         {
+            "type": "Extension.Action",
+            "version": "1.0",
+            "data": {
+               "action": "Open.Intent",
+               "url": targetUri
+            }
+         }
+      ]
+   }
+   
+    print("[response] ", json_object)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(json_object, ensure_ascii=False)
+    }
+
+def getExecUri(idx, resolve_info_list):
+        return resolve_info_list[idx]["intentList"][0]["intentInfo"]["intent"]
+    # with open("app-exec-set.json", 'r', encoding='UTF-8') as file:
+    #     appExecDataSet = json.load(file)
+    
+    # execList = appExecDataSet["actionType"][appExec]
+    
+    # for data in execList:
+    #     if data['appName'] == appName:
+    #         return data['uri'].replace('{search_keyword}', searchKeyword) if searchKeyword != "" else data['uri']
---
+++ similarity_checker.py
@@ -0,0 +1,88 @@
+from num_for_check import *
+
+class similarity_checker:
+ 
+    def get_levenshtein_distance(self, s1, s2):
+        m = len(s1)
+        n = len(s2)
+    
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            dp[i][0] = i
+        
+        for i in range(1, n+1):
+            dp[0][i] = i
+            
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                cost = 0 if s1[i-1] == s2[j-1] else 1
+                dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + cost)
+        
+        return dp[m][n]
+    
+    def get_korean_levenshtein_distance(self, s1, s2):
+        m = len(s1)
+        n = len(s2)
+
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            dp[i][0] = i
+        
+        for i in range(1, n+1):
+            dp[0][i] = i
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                cost = self.sub_cost(s1[i-1], s2[j-1])
+                dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + cost)
+        
+        return dp[m][n]
+        
+    
+    def check_korean(self, c):
+        i = ord(c)
+        return (i in range(KOR_BEGIN, KOR_END+1)) or (i in range(JAUM_BEGIN, JAUM_END+1)) or (i in range(MOUM_BEGIN, MOUM_END+1))
+    
+    def decompose(self, c):
+        if self.check_korean(c) is False:
+            return []
+        
+        i = ord(c)
+        
+        if i in range(JAUM_BEGIN, JAUM_END+1):
+            return [c, ' ', ' ']
+        
+        if i in range(MOUM_BEGIN, MOUM_BEGIN+1):
+            return [' ', c, ' ']
+        
+        i -= KOR_BEGIN
+        
+        cho = i // CHOSUNG_BASE
+        joong = (i - cho * CHOSUNG_BASE) // JOONGSUNG_BASE
+        jong = (i - cho * CHOSUNG_BASE - joong * JOONGSUNG_BASE)
+        
+        return [CHOSUNG_LIST[cho], JOONGSUNG_LIST[joong], JONGSUNG_LIST[jong]]
+    
+    def compose(self, cho, joong, jong):
+        tmp = KOR_BEGIN + CHOSUNG_LIST.index(cho)*CHOSUNG_BASE + JOONGSUNG_LIST.index(joong)*JOONGSUNG_BASE + JONGSUNG_LIST.index(jong)
+        
+        return chr(tmp)
+
+    def sub_cost(self, c1, c2):
+        if c1 == c2: return 0.0
+        else: return self.get_levenshtein_distance(self.decompose(c1), self.decompose(c2)) / 3.0
+        
+    def find_similarity(self, s1, s2):
+        if s1 == "" or s2 == "" : return -1
+        
+        result = -1
+        max_length = max(len(s1), len(s2))
+        
+        if max_length > 0:
+            result = (max_length - self.get_korean_levenshtein_distance(s1, s2)) / max_length
+        else:
+            result = 1
+        
+        return result
